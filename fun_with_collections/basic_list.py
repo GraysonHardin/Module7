50c88d4ad3093ccd62d4cf44c6f49701237c27c6
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-10-10 18:59:03 -0500

    Wrote first test (failed).

---
+++ fun_with_collections/basic_list.py
@@ -0,0 +1,19 @@
+"""
+Program: basic_list.py
+Author: Grayson Hardin
+Last date modified: 10/10/2020
+
+
+"""
+
+
+def make_list():
+ pass
+
+
+def get_input():
+    pass
+
+if __name__ == '__main__':
+   make_list()
+   get_input()
---
+++ test_collections/test_basic_list.py
@@ -0,0 +1,13 @@
+import unittest
+from unittest.mock import patch
+from fun_with_collections import basic_list
+
+
+class MyTestCase(unittest.TestCase):
+    @patch('fun_with_collections.basic_list.get_input', return_value='3')
+    def test_make_list(self, input):
+        self.assertEqual(basic_list.make_list(), [3, 3, 3])
+
+
+if __name__ == '__main__':
+    unittest.main()
