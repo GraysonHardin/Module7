482fdb5d44ef943e86ade57303f8f86d877c8e41
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-10-12 09:45:28 -0500

    Added docstring and program info.

--- search_and_sort_assignment/sort_and_search_list.py
+++ search_and_sort_assignment/sort_and_search_list.py
@@ -3,6 +3,9 @@ Program: sort_and_search.py
 Author: Grayson Hardin
 Last date modified: 10/12/2020
 
+This program accepts a search_term (such as a number). Then, gives the user an option to input three numbers and the program will return the search_term's index value. If it is not in the list, it will return -1.
+
+In addition, the program asks for another set of numbers and will print that list in a organized fashion.
 """
 
 from basic_list_exception_assignment.basic_list_exceptions import make_list
