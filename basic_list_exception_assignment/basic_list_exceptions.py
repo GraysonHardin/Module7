365f8e82946023873e9ab0a14790777528850afc
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-10-11 22:55:11 -0500

    Added all the features and all tests pass. Code works as intended.

--- basic_list_exception_assignment/basic_list_exceptions.py
+++ basic_list_exception_assignment/basic_list_exceptions.py
@@ -1,14 +1,24 @@
 """
-Program: basic_function.py
+Program: basic_list_exceptions.py
 Author: Grayson Hardin
-Last date modified: 9/28/2020
+Last date modified: 10/11/2020
 
 
 """
 
 
 def make_list():
-    pass # later logic will be added
+    my_list = []
+
+    while len(my_list) <= 2:
+        # int function will raise a value exception when it cannot cast to integer.
+        user_input = int(get_input())
+        if user_input < 1 or user_input > 50:
+            raise ValueError
+
+        my_list.append(user_input)
+
+    return my_list
 
 
 def get_input():
--- test_collections/test_basic_list_exception.py
+++ test_collections/test_basic_list_exception.py
@@ -4,12 +4,16 @@ from basic_list_exception_assignment import basic_list_exceptions
 
 
 class MyTestCase(unittest.TestCase):
+    @patch('basic_list_exception_assignment.basic_list_exceptions.get_input', return_value='5')  # This test will validate that the input is correct.
+    def test_make_list(self, input):
+        self.assertEqual(basic_list_exceptions.make_list(), [5, 5, 5])
+
     @patch('basic_list_exception_assignment.basic_list_exceptions.get_input', return_value='ab')  # This test handles non-numeric values and will return an error.
     def test_make_list_non_numeric(self, input):
         with self.assertRaises(ValueError):
             basic_list_exceptions.make_list()
 
-    @patch('basic_list_exception_assignment.basic_list_exceptions.get_input', return_value='0')  # This test will handle values below 1 and raise an error
+    @patch('basic_list_exception_assignment.basic_list_exceptions.get_input', return_value='0')  # This test will handle values below 1 and raise an error.
     def test_make_list_below_range(self, input):
         with self.assertRaises(ValueError):
             basic_list_exceptions.make_list()
