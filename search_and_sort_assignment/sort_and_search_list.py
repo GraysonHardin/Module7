8057a5b354caf3234f8dd012ef9be50cc3466b98
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-10-12 08:34:52 -0500

    Added more code to resolve three of the tests. Now, all tests pass and code works as intended.

--- search_and_sort_assignment/sort_and_search_list.py
+++ search_and_sort_assignment/sort_and_search_list.py
@@ -1,8 +1,7 @@
 """
 Program: sort_and_search.py
 Author: Grayson Hardin
-Last date modified: 10/11/2020
-
+Last date modified: 10/12/2020
 
 """
 
@@ -16,20 +15,22 @@ def search_list(search_value):
         index = my_list.index(search_value)
         return index
 
-    except ValueError:
+    except ValueError:  # If the search term is not within the list, it will return a -1
         not_in_list = -1
         return not_in_list
 
 
 def sort_list():
-    pass
+    my_list = make_list()
+    return sorted(my_list)  # I decided to add a return statement to my sort_list so it would not be void. While it could be done with or without, I prefer assigning it to a variable and then returning the variable.
 
 
 def main():
-    user_search_value = int(input("Provide an integer search term: "))
+    user_search_value = int(input("Provide an integer search term: "))  # The list will be searched with this number and will then go to lines 14-20 for validation.
     print(search_list(user_search_value))
     print('This is the sorted list', sort_list())
 
 
 if __name__ == '__main__':
     main()
+
